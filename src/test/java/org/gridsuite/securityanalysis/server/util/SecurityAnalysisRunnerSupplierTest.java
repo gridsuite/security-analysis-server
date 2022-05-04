bd189f0dd81f963d1e78bce9c1704c91b562afad
klesaulnier <42617371+archendel@users.noreply.github.com>
2022-05-04 13:10:32 +0200

    Loadflow provider default value can now be defined in application.properties (#42)
    Signed-off-by: LE SAULNIER Kevin <kevin.lesaulnier@rte-france.com>

---
+++ src/test/java/org/gridsuite/securityanalysis/server/util/SecurityAnalysisRunnerSupplierTest.java
@@ -0,0 +1,40 @@
+/**
+ * Copyright (c) 2021, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package org.gridsuite.securityanalysis.server.util;
+
+import com.powsybl.commons.PowsyblException;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author Geoffroy Jamgotchian <geoffroy.jamgotchian at rte-france.com>
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SecurityAnalysisRunnerSupplierTest {
+
+    @Value("${loadflow.default-provider}")
+    String defaultLoadflowProvider;
+
+    @Autowired
+    SecurityAnalysisRunnerSupplier securityAnalysisRunnerSupplier;
+
+    @Test
+    public void test() {
+        assertEquals("OpenSecurityAnalysis", securityAnalysisRunnerSupplier.getRunner("OpenSecurityAnalysis").getName());
+        assertEquals("Hades2", securityAnalysisRunnerSupplier.getRunner("Hades2").getName());
+        assertEquals(defaultLoadflowProvider, securityAnalysisRunnerSupplier.getRunner(null).getName());
+        PowsyblException e = assertThrows(PowsyblException.class, () -> securityAnalysisRunnerSupplier.getRunner("XXX"));
+        assertEquals("SecurityAnalysisProvider 'XXX' not found", e.getMessage());
+    }
+}
