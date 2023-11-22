59092f1f9149dcc9788b162d42bd84081f50975b
LE SAULNIER Kevin <kevin.lesaulnier@rte-france.com>
2023-11-22 16:41:19 +0100

    fix: sonar code smell
    Signed-off-by: LE SAULNIER Kevin <kevin.lesaulnier@rte-france.com>

--- src/main/java/org/gridsuite/securityanalysis/server/RestResponseEntityExceptionHandler.java
+++ src/main/java/org/gridsuite/securityanalysis/server/RestResponseEntityExceptionHandler.java
@@ -23,9 +23,7 @@ public class RestResponseEntityExceptionHandler {
         switch (exception.getType()) {
             case RESULT_NOT_FOUND :
                 return ResponseEntity.status(HttpStatus.NOT_FOUND).body(exception.getType());
-            case INVALID_FILTER_FORMAT:
-            case INVALID_FILTER:
-            case INVALID_SORT_FORMAT:
+            case INVALID_FILTER_FORMAT,INVALID_FILTER, INVALID_SORT_FORMAT:
                 return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(exception.getType());
             default:
                 return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
