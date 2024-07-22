fb9cbde816c9dcaf9dd2245b8997d34bf37ae077
Franck LECUYER <franck.lecuyer@rte-france.com>
2024-07-22 10:52:29 +0200

    Upgrade to powsybl dependencies 2024.2.0
    Signed-off-by: Franck LECUYER <franck.lecuyer@rte-france.com>

--- src/test/java/org/gridsuite/securityanalysis/server/SecurityAnalysisProviderMock.java
+++ src/test/java/org/gridsuite/securityanalysis/server/SecurityAnalysisProviderMock.java
@@ -6,19 +6,12 @@
  */
 package org.gridsuite.securityanalysis.server;
 
-import com.powsybl.commons.report.ReportNode;
-import com.powsybl.computation.ComputationManager;
 import com.powsybl.contingency.*;
 import com.powsybl.iidm.network.Network;
 import com.powsybl.iidm.network.TwoSides;
 import com.powsybl.loadflow.LoadFlowResult;
 import com.powsybl.security.*;
-import com.powsybl.action.Action;
-import com.powsybl.security.interceptors.SecurityAnalysisInterceptor;
-import com.powsybl.security.limitreduction.LimitReduction;
-import com.powsybl.security.monitor.StateMonitor;
 import com.powsybl.security.results.PostContingencyResult;
-import com.powsybl.security.strategy.OperatorStrategy;
 import org.gridsuite.securityanalysis.server.dto.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -199,17 +192,8 @@ public class SecurityAnalysisProviderMock implements SecurityAnalysisProvider {
 
     public CompletableFuture<SecurityAnalysisReport> run(Network network,
                                                          String workingVariantId,
-                                                         LimitViolationDetector detector,
-                                                         LimitViolationFilter filter,
-                                                         ComputationManager computationManager,
-                                                         SecurityAnalysisParameters parameters,
                                                          ContingenciesProvider contingenciesProvider,
-                                                         List<SecurityAnalysisInterceptor> interceptors,
-                                                         List<OperatorStrategy> operatorStrategies,
-                                                         List<Action> actions,
-                                                         List<StateMonitor> monitors,
-                                                         List<LimitReduction> limitReductions,
-                                                         ReportNode reportNode) {
+                                                         SecurityAnalysisRunParameters parameters) {
         LOGGER.info("Run security analysis mock");
         switch (workingVariantId) {
             case VARIANT_3_ID -> {
