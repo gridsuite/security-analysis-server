38cef29d1b018e1536724f32f4eeeb4c2e9faca2
AAJELLAL <ali.ajellalod@gmail.com>
2025-02-26 15:44:44 +0100

    update tests.
    Signed-off-by: AAJELLAL <ali.ajellalod@gmail.com>

--- src/test/java/org/gridsuite/securityanalysis/server/service/SecurityAnalysisResultServiceTest.java
+++ src/test/java/org/gridsuite/securityanalysis/server/service/SecurityAnalysisResultServiceTest.java
@@ -6,61 +6,31 @@
  */
 package org.gridsuite.securityanalysis.server.service;
 
-import com.github.tomakehurst.wiremock.WireMockServer;
 import com.powsybl.iidm.network.Network;
 import com.powsybl.iidm.network.test.EurostagTutorialExample1Factory;
-import com.powsybl.network.store.client.NetworkStoreService;
-import com.powsybl.network.store.client.PreloadingStrategy;
 import com.powsybl.network.store.iidm.impl.NetworkFactoryImpl;
-import com.powsybl.ws.commons.computation.service.ReportService;
-import com.powsybl.ws.commons.computation.service.UuidGeneratorService;
 import com.vladmihalcea.sql.SQLStatementCountValidator;
 import org.gridsuite.securityanalysis.server.dto.SecurityAnalysisStatus;
 import org.junit.jupiter.api.Test;
-import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 
 import java.util.UUID;
 
-import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
 import static org.gridsuite.securityanalysis.server.SecurityAnalysisProviderMock.*;
 import static org.gridsuite.securityanalysis.server.util.DatabaseQueryUtils.assertRequestsCount;
-import static org.mockito.BDDMockito.given;
 
 /**
  * @author Florent MILLOT <florent.millot@rte-france.com>
  */
 @SpringBootTest
 class SecurityAnalysisResultServiceTest {
-
-    private static final UUID NETWORK_UUID = UUID.fromString("7928181c-7977-4592-ba19-88027e4254e4");
-    private static final UUID RESULT_UUID = UUID.fromString("0c8de370-3e6c-4d72-b292-d355a97e0d5d");
     @Autowired
     private SecurityAnalysisResultService securityAnalysisResultService;
 
-    @MockBean
-    private NetworkStoreService networkStoreService;
-    @MockBean
-    private UuidGeneratorService uuidGeneratorService;
-
-    @MockBean
-    private ReportService reportService;
-
     @Test
     void deleteResultPerfTest() {
-        WireMockServer wireMockServer = new WireMockServer(wireMockConfig().dynamicPort());
-        wireMockServer.start();
-        MockitoAnnotations.initMocks(this);
-
-        // network store service mocking
         Network network = EurostagTutorialExample1Factory.create(new NetworkFactoryImpl());
-
-        given(networkStoreService.getNetwork(NETWORK_UUID, PreloadingStrategy.ALL_COLLECTIONS_NEEDED_FOR_BUS_VIEW)).willReturn(network);
-
-        // UUID service mocking to always generate the same result UUID
-        given(uuidGeneratorService.generate()).willReturn(RESULT_UUID);
         UUID resultUuid = UUID.randomUUID();
         securityAnalysisResultService.insert(network, resultUuid, RESULT, SecurityAnalysisStatus.CONVERGED);
         SQLStatementCountValidator.reset();
