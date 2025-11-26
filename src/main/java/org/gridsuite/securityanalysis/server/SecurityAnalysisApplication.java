54ffe40c033b135f6d61be4ca4fd28b7fd7f3800
Hugo Marcellin <hugo.marcelin@rte-france.com>
2025-11-26 16:41:23 +0100

    Rename commom handler to ComputationRestResponseEntityExceptionHandler

--- src/main/java/org/gridsuite/securityanalysis/server/SecurityAnalysisApplication.java
+++ src/main/java/org/gridsuite/securityanalysis/server/SecurityAnalysisApplication.java
@@ -7,7 +7,7 @@
 package org.gridsuite.securityanalysis.server;
 
 import com.powsybl.network.store.client.NetworkStoreService;
-import org.gridsuite.computation.error.RestResponseEntityExceptionHandler;
+import org.gridsuite.computation.error.ComputationRestResponseEntityExceptionHandler;
 import org.gridsuite.computation.service.NotificationService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -16,7 +16,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author Geoffroy Jamgotchian <geoffroy.jamgotchian at rte-france.com>
  */
 @SuppressWarnings("checkstyle:HideUtilityClassConstructor")
-@SpringBootApplication(scanBasePackageClasses = { SecurityAnalysisApplication.class, NetworkStoreService.class, NotificationService.class, RestResponseEntityExceptionHandler.class })
+@SpringBootApplication(scanBasePackageClasses = { SecurityAnalysisApplication.class, NetworkStoreService.class, NotificationService.class, ComputationRestResponseEntityExceptionHandler.class })
 public class SecurityAnalysisApplication {
     public static void main(String[] args) {
         SpringApplication.run(SecurityAnalysisApplication.class, args);
