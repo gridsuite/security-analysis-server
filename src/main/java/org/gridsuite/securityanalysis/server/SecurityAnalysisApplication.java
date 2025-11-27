ad472bf952752de5e5bd0f47888ae0cb8754f7c3
Hugo Marcellin <hugo.marcelin@rte-france.com>
2025-11-27 16:54:54 +0100

    Rename handlers

--- src/main/java/org/gridsuite/securityanalysis/server/SecurityAnalysisApplication.java
+++ src/main/java/org/gridsuite/securityanalysis/server/SecurityAnalysisApplication.java
@@ -8,7 +8,7 @@ package org.gridsuite.securityanalysis.server;
 
 import com.powsybl.network.store.client.NetworkStoreService;
 import com.powsybl.ws.commons.error.BaseExceptionHandler;
-import org.gridsuite.computation.error.ComputationRestResponseEntityExceptionHandler;
+import org.gridsuite.computation.error.ComputationExceptionHandler;
 import org.gridsuite.computation.service.NotificationService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -17,7 +17,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author Geoffroy Jamgotchian <geoffroy.jamgotchian at rte-france.com>
  */
 @SuppressWarnings("checkstyle:HideUtilityClassConstructor")
-@SpringBootApplication(scanBasePackageClasses = {SecurityAnalysisApplication.class, NetworkStoreService.class, NotificationService.class, ComputationRestResponseEntityExceptionHandler.class, BaseExceptionHandler.class})
+@SpringBootApplication(scanBasePackageClasses = {SecurityAnalysisApplication.class, NetworkStoreService.class, NotificationService.class, ComputationExceptionHandler.class, BaseExceptionHandler.class})
 public class SecurityAnalysisApplication {
     public static void main(String[] args) {
         SpringApplication.run(SecurityAnalysisApplication.class, args);
