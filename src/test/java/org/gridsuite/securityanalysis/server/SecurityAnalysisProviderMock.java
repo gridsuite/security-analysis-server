a5e616c637dda25cde94d716d69032001c47365e
Franck LECUYER <franck.lecuyer@rte-france.com>
2026-09-24 13:53:14 +0200

    Fix failing tests in csv export
    Signed-off-by: Franck LECUYER <franck.lecuyer@rte-france.com>

--- src/test/java/org/gridsuite/securityanalysis/server/SecurityAnalysisProviderMock.java
+++ src/test/java/org/gridsuite/securityanalysis/server/SecurityAnalysisProviderMock.java
@@ -177,7 +177,9 @@ public class SecurityAnalysisProviderMock implements SecurityAnalysisProvider {
     static final List<ContingencyResultDTO> RESULT_CONTINGENCIES_WITH_LIMIT_VIOLATIONS_ON_BOTH_SIDES = Stream.of(
             CONTINGENCIES_NOT_CONVERGED_WITHOUT_LIMIT_VIOLATION.stream().map(c -> toContingencyResultDTO(c, LoadFlowResult.ComponentResult.Status.FAILED.name(), List.of())),
             FAILED_CONTINGENCIES.stream().map(c -> toContingencyResultDTO(c, LoadFlowResult.ComponentResult.Status.FAILED.name(), FAILED_LIMIT_VIOLATIONS)),
-            CONTINGENCIES.stream().map(c -> toContingencyResultDTO(c.getContingency(), LoadFlowResult.ComponentResult.Status.CONVERGED.name(), LIMIT_VIOLATIONS_ON_BOTH_SIDES)))
+            CONTINGENCIES.stream().map(c -> toContingencyResultDTO(c.getContingency(), LoadFlowResult.ComponentResult.Status.CONVERGED.name(), LIMIT_VIOLATIONS_ON_BOTH_SIDES)),
+            CONTINGENCIES_WITHOUT_LIMIT_VIOLATION.stream().map(c -> toContingencyResultDTO(c, LoadFlowResult.ComponentResult.Status.CONVERGED.name(), List.of()))
+        )
         .flatMap(Function.identity()).toList();
 
     static List<ContingencyResultDTO> getResultContingenciesWithNestedFilter(Function<SubjectLimitViolationDTO, Boolean> filterMethod) {
